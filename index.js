71debf5f34d33eb839206f3b7902b80d7fa51620
ARAFAT05426 <arafat05426@gmail.com>
2024-05-04 16:41:04 +0600

    connected server to mongodb

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require("express");
-const app = express();
-require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
 const port = process.env.PORT || 5426;
-//MIDDLEWARE
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b5qg8rw.mongodb.net`;
+
+// MongoDB Client setup
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// Connect to MongoDB
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
+  } finally {
+    // Ensure the client will close when finished or errors occur
+    await client.close();
+  }
+}
+connectToMongoDB().catch(console.error);
+
+// Routes
 app.get("/", (req, res) => {
-    res.send("Hello NODE");
-  });
+  res.send("Hello NODE");
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
