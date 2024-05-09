b5d411746fffaf7b9dc3011366c64aa606fff3f7
ARAFAT05426 <arafat05426@gmail.com>
2024-05-09 13:01:25 +0600

    added jwt verification

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const cookieParser = require('cookie-parser');
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5426;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [''],
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
-
+app.use(cookieParser())
 // MongoDB URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b5qg8rw.mongodb.net`;
 
---
+++ verify.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token;
+
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            console.log(err);
+            return res.status(401).send({ message: 'Unauthorized access' });
+        }
+
+        console.log(decoded); // Decoded JWT payload
+        req.user = decoded; // Attach the decoded user information to the request object
+        next(); // Proceed to the next middleware or route handler
+    });
+};
+module.exports = verifyToken;
