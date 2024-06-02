4b4a4c06e5af231d29503b4e1da1054dd2736181
ARAFAT05426 <arafat05426@gmail.com>
2024-06-02 09:48:33 +0600

    added add survey functionality

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
-require("dotenv").config();
-
+const jwt = require('jsonwebtoken')
 const app = express();
 const port = process.env.PORT || 5426;
 
 // Middleware
 app.use(cors({
-  origin: [''],
+  origin: ['http://localhost:5173'],
   credentials: true,
   optionsSuccessStatus: 200
 }));
@@ -31,9 +31,52 @@ const client = new MongoClient(uri, {
 async function connectToMongoDB() {
   try {
     // await client.connect();
-    const carDoctor = client.db("carDoctor");
-    const services = carDoctor.collection("services");
+    const KwikPolls = client.db("KwikPolls");
+    const users = KwikPolls.collection("users");
+    const surveys = KwikPolls.collection("surveys");
     console.log("Connected to MongoDB!");
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res.send({ success: true, token })
+    })
+    app.put('/user', async (req, res) => {
+      const user = req.body
+
+      const query = { email: user?.email }
+      // check if user already exists in db
+      const isExist = await users.findOne(query)
+      if (isExist) {
+        if (user.status === 'Requested') {
+          // if existing user try to change his role
+          const result = await users.updateOne(query, {
+            $set: { status: user?.status },
+          })
+          return res.send(result)
+        } else {
+          // if existing user login again
+          return res.send(isExist)
+        }
+      }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      } 
+      const result = await users.updateOne(query, updateDoc, options)
+      res.send(result)
+    })
+    // SURVEY
+    app.post('/survey', async(req, res) =>{
+      const data = req.body;
+      const result = await surveys.insertOne(data)
+      res.send(result)
+    })
+
   } finally {
     // Ensure the client will close when finished or errors occur
     // await client.close();
@@ -43,7 +86,7 @@ connectToMongoDB().catch(console.error);
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Hello NODE");
+  res.send("Hello KwickPoll");
 });
 
 // Start server
